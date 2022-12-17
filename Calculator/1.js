7d29e2fc14362e075235acfa393015232aa16288
MightySeswatha <teszerorobot@gmail.com>
2022-12-17 11:43:04 +0300

    'v1'

--- Calculator/1.js
+++ Calculator/1.js
@@ -1,3 +1,15 @@
 window.onload = function(){
-  
+
+let elem = document.getElementsByClassName('elem');
+let elem_del = document.getElementsByClassName('elem_del');
+
+for(let i = 0; i < elem.length; i++){
+
+elem[i].onfocus = function(){
+elem[i].style.backgroundColor = "black";
+elem[i].style.color = "white";
+}
+
+}
+
 }
