c00100e69ffd5602b890804dd1b99b8e7910d38b
MightySeswatha <teszerorobot@gmail.com>
2022-12-23 14:33:47 +0300

    new6

--- Test3/1.js
+++ Test3/1.js
@@ -1,71 +1,65 @@
-window.onload = function(){
+  window.onload = function(){
 
-let btn = document.getElementById("btn");
-let radio = document.getElementsByClassName('radio');
-let text = document.getElementsByClassName('text');
+  let btn = document.getElementById("btn");
+  let radio = document.getElementsByClassName('radio');
+  let text = document.getElementsByClassName('text');
 
-let sum1 = 0;
-let sum2 = 0;
-let sum3 = 0;
-let arr = [];
+  let sum1 = 0;
+  let sum2 = 0;
+  let sum3 = 0;
+  let arr = [0,0,0,0,0,0,0,0,0,0,0,0];
+  let t = 0;
+
+  btn.onclick = function(){
 
 
+  if(t == 0){
 
-for(let i = 0; i < radio.length; i++){
-radio[i].onclick = function(){
-/*Закончил здесь*/
-radio[i].value = Number(radio[i].value)+1;
 
-if(i >= 0 && i <= 4){
-let i1 = i;
-for(let j = 0; j <= 4; j++){
-if(j != i1){radio[j].checked = false;}
-}
-sum1++;
-}
-else if(i >= 5 && i <= 7){
-let i1 = i;
-for(let j = 5; j <= 7; j++){
-if(j != i1){radio[j].checked = false;}
-}
-sum2++;
-}
-else if(i >= 8 && i <= 11){
-let i1 = i;
-for(let j = 8; j <= 11; j++){
-if(j != i1){radio[j].checked = false;}
-}
-sum3++;
-}
+  for(let i = 0; i < radio.length; i++){
+  if(radio[i].checked != false){
+  if(i >= 0 && i <= 4 && text[0].value.length == 0){sum1++; arr[i]++;}
+  else if(i >= 5 && i <= 7 && text[1].value.length == 0){sum2++; arr[i]++;}
+  else if(i >= 8 && i <= 11 && text[2].value.length == 0){sum3++; arr[i]++;}
+  }
+  }
 
-/**/
-}
-}
+  for(let i = 0; i < arr.length; i++){
+  if(arr[i] != 0){
+    if(i >= 0 && i <= 4 && text[0].value.length == 0){radio[i].parentElement.innerHTML = radio[i].parentElement.innerHTML.substring(0,radio[i].parentElement.innerHTML.lastIndexOf('"')+2) + radio[i].value + "("+Math.floor(arr[i]*100/sum1)+"%)";}
+    else if(i >= 5 && i <= 7 && text[1].value.length == 0){radio[i].parentElement.innerHTML = radio[i].parentElement.innerHTML.substring(0,radio[i].parentElement.innerHTML.lastIndexOf('"')+2) + radio[i].value + "("+Math.floor(arr[i]*100/sum2)+"%)";}
+    else if(i >= 8 && i <= 11 && text[2].value.length == 0){radio[i].parentElement.innerHTML = radio[i].parentElement.innerHTML.substring(0,radio[i].parentElement.innerHTML.lastIndexOf('"')+2) + radio[i].value + "("+Math.floor(arr[i]*100/sum3)+"%)";}
+  }
+  }
 
-let t = 0;
-/*Здесь тоже закончил*/
-btn.onclick = function(){
-console.log(sum1);
-console.log(sum2);
-console.log(sum3);
- if(t == 0){
+    btn.value = "Пройти опрос ещё раз";
+    t = 1;
 
-  btn.value = "Пройти опрос ещё раз";
-  t = 1;
-   for(let i = 0; i < radio.length; i++){
-   radio[i].classList.add("off");
-   radio[i].checked = false;
+     for(let i = 0; i < radio.length; i++){
+     radio[i].classList.add("off");
+     radio[i].checked = false;
+     }
+
    }
 
- }
+   else if(t == 1){
 
- else if(t == 1){
-  btn.value = "Проверить";
-  t = 0;
-   for(let i = 0; i < radio.length; i++){
-   radio[i].classList.remove("off");
+    btn.value = "Проверить";
+    t = 0;
+
+     for(let i = 0; i < radio.length; i++){
+     radio[i].classList.remove("off");
+     radio[i].checked = false;
+     }
+
+
+
    }
- }
-}
-/**/
-}
+   
+   for(let i = 0; i < text.length; i++){
+   text[i].value = "";
+   }
+
+  }
+
+  }
