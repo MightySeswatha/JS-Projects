3a5a2f8f34a73004b5feb8c5edebd00e26e01eaf
MightySeswatha <teszerorobot@gmail.com>
2022-12-26 20:30:28 +0300

    new10

--- Calendar2/1.js
+++ Calendar2/1.js
@@ -6,12 +6,75 @@ let btn_next = document.getElementById('btn_next');
 let btn_prev = document.getElementById('btn_prev');
 let text = document.getElementById('text');
 let month = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
+let row = document.getElementsByClassName('row3');
+let calendar = document.getElementById("calendar");
 
 
 changeDate(today);
 
+
+
+
 function changeDate(date){
+/*Изменение даты*/
+
+while (calendar.firstChild) {
+  calendar.removeChild(calendar.firstChild);
+}
+
+Date.prototype.daysInMonth = function() {
+    return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
+};
+
 text.innerHTML = date.getFullYear()+" "+month[date.getMonth()];
+let k = date.daysInMonth();
+console.log(k + " всего дней");
+
+
+
+let temp = new Date(date).setDate(1);
+let t = new Date(temp).getDay();
+
+let i = 1;
+
+while(i < k){
+
+let row = document.createElement("div");
+row.classList.add("row");
+row.classList.add("row3");
+
+for(let j = 0; j < 7; j++){
+
+let elem = document.createElement("div");
+elem.classList.add("elem");
+
+if(calendar.children.length == 0 && t == 0 && j < 6){elem.innerHTML = "";}
+
+else if(calendar.children.length == 0 && j+1 < t){
+elem.innerHTML = "";
+}
+
+else{
+elem.innerHTML = i;
+if(i > k){elem.innerHTML = "";}
+i++;
+
+}
+
+row.appendChild(elem);
+
+}
+
+calendar.appendChild(row);
+
+}
+
+
+
+
+
+
+
 }
 /*Переключение календаря*/
 
