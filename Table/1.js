41ab42d63c8a1e0e66fd286022d7eb41f265dc17
MightySeswatha <teszerorobot@gmail.com>
2022-12-30 14:27:54 +0300

    new18

--- Table/1.js
+++ Table/1.js
@@ -2,6 +2,8 @@ window.onload = function(){
 
 let main = document.getElementsByClassName('main');
 let k = 0;
+let tr = document.getElementsByTagName("tr");
+let table = document.getElementsByTagName("table");
 
 for(let i = 0; i < main[0].children.length; i++){
 
@@ -49,34 +51,64 @@ let arr = [];
 
 let tr = document.getElementsByTagName('tr');
 for(let j = 1; j < tr.length; j++){
-arr[j-1] = tr[j].children[i].innerHTML;
+let obj = {};
+obj[tr[j].children[i].innerHTML] = j;
+arr[j-1] = obj;
 }
 
-
+let res = [];
 
 if(k == 0){
-arr = arr.sort((a, b) => b - a);
 
-for(let j = 1; j < tr.length; j++){
-tr[j].children[i].innerHTML = arr[j-1];
+let arr1 = [];
+for(let j = 0; j < arr.length; j++){
+arr1[j] = Object.keys(arr[j])[0];
 }
+
+arr1 = arr1.sort((a, b) => b - a);
+console.log(arr1);
+let x = 0;
+
+for(let j1 = 0; j1 < arr.length; j1++){
+for(let j2 = 0; j2 < arr1.length; j2++){
+if(arr[j1][arr1[j2]] != undefined){res[x] = j2+1; x++}
+}
+}
+
+console.log(res);
 
 }
 
 else{
-arr = arr.sort();
 
-for(let j = 1; j < tr.length; j++){
-tr[j].children[i].innerHTML = arr[j-1];
+let arr1 = [];
+for(let j = 0; j < arr.length; j++){
+arr1[j] = Object.keys(arr[j])[0];
+}
+
+arr1 = arr1.sort();
+
+let x = 0;
+
+
+for(let j1 = 0; j1 < arr.length; j1++){
+for(let j2 = 0; j2 < arr1.length; j2++){
+if(arr[j1][arr1[j2]] != undefined){res[x] = j2+1; x++}
+}
+}
+
+
+
+}
+
+
+for(let i1 = 1; i1 < tr.length; i1++){
+console.log(tr[res[i1-1]].innerHTML);
 }
 
 }
 
 
 
-
-
-
-}
 
 }
