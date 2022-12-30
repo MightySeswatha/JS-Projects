3e9468595974957f49b8f2de7e757fc75f8449bb
MightySeswatha <teszerorobot@gmail.com>
2022-12-30 11:10:06 +0300

    new16

--- Menu2/1.js
+++ Menu2/1.js
@@ -1,14 +1,19 @@
 window.onload = function(){
 
 let elem = document.getElementsByClassName("elem");
+let tab = document.getElementsByClassName('tab');
 
 for(let i = 0; i < elem.length; i++){
 
 elem[i].onclick = function(){
 
 elem[i].classList.add("active");
+tab[i].classList.add("on");
+tab[i].style.height = "100%";
 for(let j = 0; j < elem.length; j++){
-if(i != j){elem[j].classList.remove("active");}
+if(i != j){elem[j].classList.remove("active");
+tab[j].classList.remove("on");
+tab[j].style.height = "0";}
 }
 }
 
